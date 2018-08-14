010f4ed3cdd36b6d800efa1e6b268a845e58dacf
nebulak <nebulak@nebulak.localhost>
2018-08-14 14:38:20 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 
-if [ ! -f /usr/local/bin/orjail ]; then
+if [ ! -f /usr/bin/torjail ]; then
     git clone https://github.com/orjail/orjail.git
-    cp ./orjail/orjail /usr/local/bin/torjail
+    cp ./orjail/orjail /usr/local/bin/orjail
 fi
 
 cp ./onion-gajim.desktop /usr/share/applications/onion-gajim.desktop
+chmod +x ./onion-gajim /usr/local/bin/orjail
 cp ./onion-gajim /usr/local/bin/onion-gajim
+chmod +x ./onion-gajim /usr/local/bin/onion-gajim
