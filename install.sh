dec439ca920cc876bf43cbe81c8dada4c78c6fa1
nebulak <nebulak@users.noreply.github.com>
2018-08-14 13:46:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 
 if [ ! -f /usr/bin/torjail ]; then
     git clone https://github.com/torjail/torjail.git
-    cp ./torjail/torjail /usr/bin/torjail
+    cp ./torjail/torjail /usr/local/bin/torjail
 fi
 
 cp ./onion-gajim.desktop /usr/share/applications/onion-gajim.desktop
-cp ./onion-gajim /usr/bin/onion-gajim
+cp ./onion-gajim /usr/local/bin/onion-gajim
